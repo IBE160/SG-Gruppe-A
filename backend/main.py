a7d5d394799751c23f7dfaace04da1ddca8ec689
astridgrepstad <astrid.grepstad04@gmail.com>
2025-12-07 16:30:10 +0100

    fungerende backend 2.0

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,21 @@
+import os
 from fastapi import FastAPI
+from supabase import create_client, Client
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = FastAPI()
+
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_KEY")
+supabase: Client = create_client(url, key)
 
 @app.get("/")
 def read_root():
     return {"Hello": "World Astrid"}
+
+@app.get("/todos")
+def get_todos():
+    response = supabase.table("todos").select("*").execute()
+    return response.data
